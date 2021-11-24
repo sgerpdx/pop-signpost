dd38712e63a7857651b44fbcadf80db6e6557507
SamuelHGerber <henrylightfoot@gmail.com>
2021-11-24 08:06:48 -0800

    intro component refactored with styles module

---
+++ components/intro.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styles from "../styles/Intro.module.css";
+// import logo images
+
+export default function Intro() {
+  return (
+    <>
+      <section className={styles.introTextArea}>
+        <p>
+          I'm Sam, thanks for stopping by! I build full-stack applications with
+          a focus on inspiring, accessible UX and I enjoy opportunities to
+          explore project-based solutions, adding new languages and tech to my
+          primary React-Node-SQL stack.
+        </p>
+        <p>
+          While my full site here is under construction, you can check out
+          projects, contact info and more using the menu below. Have a nice day!
+        </p>
+      </section>
+      <section className={styles.socialMediaArea}>
+        <p>
+          <a href="https://github.com/sgerpdx" target="_blank">
+            <Image src={LogoGitHub} alt="github logo" />
+          </a>
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/sam-h-gerber/" target="_blank">
+            <Image src={LogoLinkedin} alt="linkedin logo" />
+          </a>
+        </p>
+        <p>
+          <a href="https://twitter.com/henrylightcode" target="_blank">
+            <Image src={LogoTwitter} alt="twitter logo" />
+          </a>
+        </p>
+        <p>
+          <a href="/gerber-sam_resume-current.pdf" target="_blank">
+            <button>Resume</button>
+          </a>
+        </p>
+      </section>
+      <p>
+        <a href="https://github.com/sgerpdx" target="_blank">
+          <Image src={LogoGitHub} alt="github logo" />
+        </a>
+      </p>
+      <p>
+        <a href="https://www.linkedin.com/in/sam-h-gerber/" target="_blank">
+          <Image src={LogoLinkedin} alt="linkedin logo" />
+        </a>
+      </p>
+      <p>
+        <a href="https://twitter.com/henrylightcode" target="_blank">
+          <Image src={LogoTwitter} alt="twitter logo" />
+        </a>
+      </p>
+      <p>
+        <a href="/gerber-sam_resume-current.pdf" target="_blank">
+          <button>Resume</button>
+        </a>
+      </p>
+    </>
+  );
+}
