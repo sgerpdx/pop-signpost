1b8d06bf64f54d54d2faa53ea2ceb26fdcd7178e
SamuelHGerber <henrylightfoot@gmail.com>
2021-11-27 14:14:41 -0800

    chakra-ui dependencies installed

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/Header.module.css";
 import Image from "next/image";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 // Personal Logo:
 import PlanetImage from "../public/blue-planet-01.png";
@@ -8,18 +9,22 @@ import PlanetImage from "../public/blue-planet-01.png";
 export default function Header() {
   return (
     <>
-      <div className={styles.logoImageArea}>
-        <Image
-          src={PlanetImage}
-          alt="logo image of a blue planet with golden-brown ring"
-          height="53"
-          width="75"
-        />
-      </div>
-      <div className={styles.headlineTextArea}>
-        <h1>hello : )</h1>
-      </div>
-      <div className={styles.upperRightArea}></div>
+      <section className={styles.mainTitleArea}>
+        <div className={styles.logoImageArea}>
+          <Image
+            src={PlanetImage}
+            alt="logo image of a blue planet with golden-brown ring"
+            height="53"
+            width="75"
+          />
+        </div>
+        <div className={styles.headlineTextArea}>
+          <h1>hello : )</h1>
+        </div>
+      </section>
+      <section className={styles.mainMenuArea}>
+        <HamburgerIcon w={24} h={24} />
+      </section>
     </>
   );
 }
