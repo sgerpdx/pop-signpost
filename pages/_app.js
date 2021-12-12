c8d8d36a0a5d5f07094e6d5c59ab6645d7ca4f1e
SamuelHGerber <henrylightfoot@gmail.com>
2021-12-11 20:14:57 -0800

    code cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import "../styles/globals.css";
+//currently provider is no longer being used
 //import Provider from "../state/provider";
 
 function MyApp({ Component, pageProps }) {
+  //
+  //this component needs refactoring and DRYer code
+  //deactivating the modal can be achieved using just 'entered'
+  //'stage' is only needed in order to track how many times the user has been on the home (intro) screen for example, possibly to toggle or update the text to something fun pulled from an API...
+  //
+
   const [stage, setStage] = useState(0);
   const [entered, setEntered] = useState(false);
 
