e1b5d0020ac83ece090f8684060de7e487bb4288
SamuelHGerber <henrylightfoot@gmail.com>
2021-12-14 20:18:33 -0800

    contact link added to about

--- components/about.jsx
+++ components/about.jsx
@@ -3,26 +3,30 @@ import styles from "../styles/About.module.css";
 import Image from "next/image";
 //import Link from 'next/link';
 
-export default function About() {
+export default function About({ onClick }) {
   return (
     <>
       <section className={styles.aboutMeArea}>
-        <Image
-          src="http://placekitten.com/200/300"
-          width="100px"
-          height="150px"
-        />
+        <div className={styles.aboutHeader}>
+          <h4>About Me</h4>
+          <Image
+            src="http://placekitten.com/200/300"
+            width="100px"
+            height="150px"
+          />
+        </div>
+
         <p>
           Hi, I&apos;m Sam! I love discovering the amazing connections between
           things, and I think that one of the best things about tech is its
-          potential to bring us together, to inspire, to shorten the distance in
-          all sorts of ways.
+          potential to bring us together, to shorten the distance in all sorts
+          of ways.
         </p>
         <p>
           Along the road to software development, I&apos;ve been an account and
           studied linguistics, with a lifelong love a languages. I&apos;m
-          energized by the unprecedentedly universal nature of coding languages,
-          and how a great app can reach users anywhere in the world.
+          energized by the universal nature of coding languages, and how a great
+          app can reach users anywhere in the world.
         </p>
         <p>
           I was born in and reside in Oregon, grew up in Alaska in between, and
@@ -30,12 +34,13 @@ export default function About() {
           mountains when I have the chance, as well as reading and writing
           fiction in my free time. Coffee, music and random humor also figure
           in. Feel free to{" "}
-          <a
-            href="http://placekitten.com/200/300"
+          <button
             className={styles.contactLink}
+            onClick={onClick}
+            value="contact"
           >
             send me a line
-          </a>{" "}
+          </button>{" "}
           if you&apos;d like!
         </p>
       </section>
