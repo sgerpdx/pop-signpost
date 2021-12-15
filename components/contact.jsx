8bc8e76bed2d260b85cb4d27e7cde8aa4bb8afe7
SamuelHGerber <henrylightfoot@gmail.com>
2021-12-15 10:14:14 -0800

    text + links + styling for contact component

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,22 +1,61 @@
 import React from "react";
 import styles from "../styles/Contact.module.css";
+import Image from "next/image";
+//
+import StarsImage from "../public/stars.png";
+import AsteroidImage from "../public/asteroid-blue.png";
 
 export default function Contact() {
   return (
     <>
       <section className={styles.contactMeArea}>
-        <h4>send email</h4>
-        <input type="text" placeholder="type here" />
-        <button>submit</button>
+        <h2>Contact Details</h2>
+
+        {/* concerning email */}
         <p>
-          <a
-            href="/gerber-sam_resume-current.pdf"
-            target="_blank"
-            rel="noreferrer"
+          <span
+            style={{
+              color: "rgba(113, 192, 187, 255)",
+              fontSize: "1.2em",
+              marginRight: "10px",
+            }}
           >
-            <button>Resume</button>
+            ✉
+          </span>
+          <a
+            style={{ textDecoration: "underline" }}
+            href="mailto:samhgerber@protonmail.com"
+            title="mailto:samhgerber@protonmail.com"
+          >
+            send me an email
           </a>
         </p>
+
+        {/* concerning social media */}
+        <div className={styles.socialMediaTextArea}>
+          <figure style={{ marginRight: "10px" }}>
+            {" "}
+            <Image src={StarsImage} width="32px" height="32px" />
+          </figure>
+
+          <p style={{ width: "240px", textAlign: "left" }}>
+            {" "}
+            check out my social media links (below the cube){" "}
+          </p>
+        </div>
+
+        {/* concerning resume access */}
+        <div className={styles.resumeLinkArea}>
+          <p>
+            <a
+              href="/gerber-sam_resume-current.pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className={styles.resumeButtonContact}> resume </button>{" "}
+            </a>
+          </p>
+        </div>
       </section>
     </>
   );
