bf5b9d8fc2715c2c7e1027b988989ea1dc71de3b
SamuelHGerber <henrylightfoot@gmail.com>
2021-12-22 12:25:30 -0800

    individual projects and project text refactored out of Projects page container

---
+++ components/individualProjects/designasaur.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Designasaur() {
+  return (
+    <>
+      <p>
+        Simulated DNA laboratory allowing users to custom-engineer new dinosaur
+        hybrids and observe their behavior in a virtual park. Primarily
+        mob-programmed in a one-week remote sprint, using:
+      </p>
+      <ul>
+        <li>Vanilla JavaScript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
+      <p>
+        My role: I originated the app concept; handled composite image rendering
+        via the DOM; set up avatar selection and thematically-styled sign-in on
+        the homepage; and created graphics assets.
+      </p>
+    </>
+  );
+}
