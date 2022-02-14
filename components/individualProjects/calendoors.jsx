83b2df62a3eabb9adf53f653f1ebb6e9e2c34522
SamuelHGerber <henrylightfoot@gmail.com>
2022-02-14 14:01:19 -0800

    project description updates

--- components/individualProjects/calendoors.jsx
+++ components/individualProjects/calendoors.jsx
@@ -4,13 +4,13 @@ export default function Calendoors() {
   return (
     <>
       <p>
-        A &apos;daily-reveal calendar app&apos; that is an outgrowth of a
-        digital Advent Calendar that I made as a gift this past December. It
-        will allow the user to create an account and set up customized calendars
-        that facilitate content-reveals at scheduled intervals, e.g.
-        today&apos;s 'door' can be opened by the recipient-user, who will then
-        be able to access whatever information or media the admin-user has
-        loaded behind that door.
+        A &apos;daily-reveal calendar app&apos; that&apos;s an outgrowth of a
+        digital Advent Calendar I built as a Christmas gift. It will allow the
+        user to create an account and set up customized calendars that
+        facilitate content-reveals at scheduled intervals, e.g. today&apos;s
+        'door' can be opened by the recipient-user, who will then be able to
+        access whatever information or media the admin-user has loaded behind
+        that door.
       </p>
       <ul>
         <li>Front-end: React + Nextjs</li>
